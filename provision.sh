ced74cfc9adef6ff91d7c5e14b0146e2c2e7d3b9
Robert <robert_zibert@hotmail.com>
2017-05-04 19:16:46 -0300

    Changing swap

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 # This script will run inside docker machine
 composer self-update
-/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=512
 /sbin/mkswap /var/swap.1
 /sbin/swapon /var/swap.1
 cd share
