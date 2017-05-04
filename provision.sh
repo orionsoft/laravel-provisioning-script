ccbe2a59bf6256da103fd0977ac3e887e067e0e8
Robert <robert_zibert@hotmail.com>
2017-05-04 19:14:59 -0300

    Changes on composer

--- provision.sh
+++ provision.sh
@@ -1,8 +1,9 @@
 # This script will run inside docker machine
+composer self-update
 /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 /sbin/mkswap /var/swap.1
 /sbin/swapon /var/swap.1
 cd share
-composer install
+composer install --prefer-dist
 chmod -R o+w storage/
 php artisan migrate --force
