4aa7061b0eb6ec75e33a7aa4a591a99dd4fd976a
Robert <robert_zibert@hotmail.com>
2017-05-09 11:23:59 -0300

    adding build

---
+++ build.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+
+# Installing Docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
+
+# Installing Docker Container
+sudo docker pull mtmacdonald/docker-laravel:1.4.0
--- start.sh
+++ start.sh
@@ -1,23 +1,7 @@
-sudo apt-get update
-
-# Installing Docker
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
-sudo apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
-
-# Installing Docker Container
-sudo docker pull mtmacdonald/docker-laravel:1.4.0
-
-/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-/sbin/mkswap /var/swap.1
-/sbin/swapon /var/swap.1
-
-
 # Moving Enviroment variables
 sudo cp /home/deploy/.env  /home/deploy/app/.env
 sudo cp /home/deploy/script/provision.sh  /home/deploy/app/provision.sh
+
 # Run Docker container
 sudo docker run -d \
   -v /home/deploy/app:/share \
@@ -26,6 +10,7 @@ sudo docker run -d \
   --restart=always \
   mtmacdonald/docker-laravel:1.4.0
 ls /home/deploy/app
+
 echo "----------------ward--------------------"
 # Running provision command inside Docker
 sudo docker exec -i laravel bash /share/provision.sh
