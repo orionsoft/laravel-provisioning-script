efa5c86779292c363c17225546166a7ef12f9ab1
Robert <robert_zibert@hotmail.com>
2017-05-09 12:35:45 -0300

    fixing errors

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,6 +1,6 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
-  sudo docker exec -i tail -f share/storage/logs/laravel.log -f >> /home/deploy/app.log
+  sudo docker exec -i laravel tail share/storage/logs/laravel.log -f >> /home/deploy/app.log
   echo "\n" >> /home/deploy/app.log
   sleep 2s
 done
