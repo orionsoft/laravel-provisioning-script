817e1a08f60b88a4e38c43e06e49b629fe017e60
Robert <robert_zibert@hotmail.com>
2017-05-09 16:27:17 -0300

    fixing

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 # This script will run inside docker machine
 cd share
 composer self-update
-sudo chmod -R ug+rwx storage bootstrap/cache
+chmod -R ug+rwx storage bootstrap/cache
 composer install --prefer-dist
