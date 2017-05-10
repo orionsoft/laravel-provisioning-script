e281754696b072bbb82407fc21d65c1b8df5519a
Robert <robert_zibert@hotmail.com>
2017-05-10 13:37:57 -0300

    fixing redundant commands

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,6 +1,7 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
-  sudo docker logs -f --tail laravel >> /home/deploy/app.log
+  sudo docker logs laravel --since 2s >> /home/deploy/app.log
+  #sudo docker logs -f --tail laravel >> /home/deploy/app.log
   echo "\n" >> /home/deploy/app.log
   sleep 2s
 done
--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
 # Adding Swap
-/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-/sbin/mkswap /var/swap.1
-/sbin/swapon /var/swap.1
+# /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+# /sbin/mkswap /var/swap.1
+# /sbin/swapon /var/swap.1
 
-sudo docker run -d \
-  -v /home/deploy/app:/share \
-  -p 80:80 \
-  --name laravel \
-  --restart=always \
-  mtmacdonald/docker-laravel:1.4.0
 # Running provision command inside Docker
 sleep 5s
-# nohup sh /home/deploy/script/pipeLogs.sh > /dev/null 2>&1 &
+nohup sh /home/deploy/script/pipeLogs.sh > /dev/null 2>&1 &
